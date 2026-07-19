c24e425774889dd8aeae32128861590415d5b350
Alex Todua <alx.todua777@gmail.com>
2026-07-20 00:23:31 +0400

    Adding FIles

--- FirstAngularProject/src/app/app.ts
+++ FirstAngularProject/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
+import { Header } from './header/header';
+import { Main } from './main/main';
+import { Footer } from './footer/footer';
 
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Header,Main,Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
